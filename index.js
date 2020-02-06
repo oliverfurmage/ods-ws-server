66cc32e6098bcac4c89a298ae0fe7d4413e225fa
Ollie Furmage <ollie@nettex.co.uk>
2020-02-06 17:09:56 +0000

    Few comments to make it easier to understand.

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({
   port: 2222
 });
- 
+
 wss.on('connection', function connection(ws, req, client) {
+
+    // log connection
     console.log(`Connection {client: ${client},ip: ${req.connection.remoteAddress}}`);
 
     ws.on('message', function incoming(message) {
-      console.log('received: %s', message);
+        // log message
+      console.log(`Message Recieved: ${message}`);
+      // forward to clients connected
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
           client.send(message);
